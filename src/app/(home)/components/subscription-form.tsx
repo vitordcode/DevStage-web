3d23de51aed96cf27f8a985142de4334a8fb52d5
Vitor de Paula <vitordepaula98@hotmail.com>
2025-02-23 20:19:39 -0300

    feat: instalado Orval para consumir API do docker, aplicacao finalizada

--- src/app/(home)/components/subscription-form.tsx
+++ src/app/(home)/components/subscription-form.tsx
@@ -1,6 +1,8 @@
 'use client'
+import { subscribeToEvent } from '@/http/api'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight, Mail, User } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Button } from '../../components/button'
@@ -14,6 +16,9 @@ const subscriptionSchema = z.object({
 type SubscriptionSchema = z.infer<typeof subscriptionSchema>
 
 export function SubscriptionForm() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
   const {
     register,
     handleSubmit,
@@ -22,8 +27,12 @@ export function SubscriptionForm() {
     resolver: zodResolver(subscriptionSchema),
   })
 
-  function onSubscribe(data: SubscriptionSchema) {
-    console.log(data)
+  async function onSubscribe({ name, email }: SubscriptionSchema) {
+    const referrer = searchParams.get('referrer')
+
+    const { subscriberId } = await subscribeToEvent({ name, email, referrer })
+
+    router.push(`/invite/${subscriberId}`)
   }
 
   return (
---
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -0,0 +1,54 @@
+import {
+  getSubscriberInviteClicks,
+  getSubscriberInviteCount,
+  getSubscriberRankingPosition,
+} from '@/http/api'
+import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
+
+interface StatsProps {
+  subscriberId: string
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+  const { count: accesCount } = await getSubscriberInviteClicks(subscriberId)
+  const { count: inviteCount } = await getSubscriberInviteCount(subscriberId)
+  const { position: rankingPosition } =
+    await getSubscriberRankingPosition(subscriberId)
+
+  return (
+    <div className="grid gap-3 md:grid-cols-3">
+      <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {accesCount}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">
+          Acesso ao link
+        </span>
+
+        <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
+      </div>
+
+      <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {inviteCount}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">
+          Inscrições feitas
+        </span>
+
+        <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
+      </div>
+
+      <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {rankingPosition ? `${rankingPosition}°` : '-'}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">
+          Posição no ranking
+        </span>
+
+        <Medal className="size-5 text-purple absolute top-3 left-3" />
+      </div>
+    </div>
+  )
+}
